ede8aa2b28a9bd254a7b07c34178cf8cec0832d8
Nightz <peerapat.ud@gmail.com>
2022-05-02 16:42:45 +0700

    Make UI display changing length

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 const gridContainer = document.querySelector(".grid-container");
 const gridLength = document.querySelector(".grid-length");
 const btnApply = document.querySelector(".btn-apply");
-let length = gridLength.value;
-console.log(length);
+const gridSize = document.querySelector(".grid-size");
 
+btnApply.addEventListener('click', recreateGrid);
+gridLength.addEventListener('change',changeLength);
 
-function createGrid(length=16) {
+function createGrid(length) {
     for (let i = 0; i < length ** 2 ; i++) {
         const box = document.createElement("div");
         gridContainer.appendChild(box);
-        // console.count(box);
     }
-    console.log(gridContainer);
     gridContainer.style.gridTemplateColumns = `repeat(${length},auto)`;
 }
-createGrid(length);
+
+function removeGrid(container) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+function getLength() {
+    let length = gridLength.value;
+    console.log(length);
+    return length;
+}
+
+function recreateGrid() {
+    removeGrid(gridContainer);
+    createGrid(getLength());
+}
+
+function changeLength() {
+    gridSize.textContent = `${getLength()} x ${getLength()}`;
+}
+
+createGrid(16);
